00c7b0df9186121aca3f7f94a7ead5c967432318
Mayro <deehmyller11@gmail.com>
2021-07-02 20:32:14 -0300

    refact customer components

--- src/components/Customer/index.js
+++ src/components/Customer/index.js
@@ -1,176 +1,167 @@
-import { useState } from 'react'
-import { Table, Collapse, Nav, TabContent, TabPane, Row, Col } from 'reactstrap'
+import { useState, useMemo } from 'react'
+import { Nav, TabContent, TabPane, Row, Col } from 'reactstrap'
+
+import DataTable from 'react-data-table-component'
 
 import * as S from './style'
 
 import CustomerDetails from '../CustomerDetails'
 import Label from '../Label/Label'
 
-import { GET_CUSTOMERS } from '../../service/api'
-import { useFetch } from '../../hooks/useFetch'
 import dateFormat from '../../helpers/DateFormat'
 
 import Image from '../../assets/images/customers-image.png'
 import { ReactComponent as BlockButtonIcon } from '../../assets/icons/block.svg'
 
-const CustomerInfo = ({ customer, handleBlock }) => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const toggle = () => setIsOpen(!isOpen)
-
+const CustomerInfo = (props) => {
   const [activeTab, setActiveTab] = useState('1')
+
+  const { data, handleBlock } = props
 
   const tabs = (tab) => {
     if (activeTab !== tab) setActiveTab(tab)
   }
 
   let customerAddress
-  if (customer.address) {
-    const { address } = customer.address
+  if (data.address) {
+    const { address } = data.address
     customerAddress = address
   }
 
-  const [date] = dateFormat(customer.created_at).split(' ')
-
   return (
     <>
-      <tr
-        onClick={toggle}
-        style={{
-          background: `${isOpen === true ? '#fff' : '#fff'}`,
-          borderBottom: '1px solid #CFCFCF',
-          cursor: 'pointer'
-        }}
-      >
-        <td className="align-middle">
-          <CustomerDetails Image={Image} name={customer.name} />
-        </td>
-        <td className="align-middle">{customer.email}</td>
-        <td className="align-middle">{customer.phone}</td>
-        <td className="align-middle">
+      <S.Container>
+        <>
+          {data.status === 'blocked' ? (
+            <></>
+          ) : (
+            <div>
+              <S.BlockButton onClick={() => handleBlock(data.id)}>
+                <BlockButtonIcon />
+                Block
+              </S.BlockButton>
+            </div>
+          )}
+        </>
+
+        <Nav tabs>
           <div>
-            <p>{date.replaceAll('-', '/')}</p>
+            <S.Button
+              className={activeTab === '1' ? 'active' : ''}
+              onClick={() => {
+                tabs('1')
+              }}
+            >
+              Profile
+            </S.Button>
           </div>
-        </td>
-        <td className="align-middle col-md-2">2</td>
-
-        <td style={{ width: '40px' }}>
-          <svg width="18" height="11" fill="none">
-            <path
-              d="M2 2L9 9L16 2"
-              stroke="#2A3642"
-              strokeWidth="3"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </td>
-      </tr>
-      <Collapse tag="tr" isOpen={isOpen} style={{ background: '#fff' }}>
-        <td colSpan="6" style={{ borderBottom: '1px solid #969696' }}>
-          <S.Container>
-            <>
-              {customer.status === 'blocked' ? (
-                <></>
-              ) : (
-                <div>
-                  <S.BlockButton onClick={() => handleBlock(customer.id)}>
-                    <BlockButtonIcon />
-                    Block
-                  </S.BlockButton>
-                </div>
-              )}
-            </>
-
-            <Nav tabs>
-              <div>
-                <S.Button
-                  className={activeTab === '1' ? 'active' : ''}
-                  onClick={() => {
-                    tabs('1')
-                  }}
-                >
-                  Profile
-                </S.Button>
-              </div>
-              <div>
-                <S.Button
-                  className={activeTab === '2' ? 'active' : ''}
-                  onClick={() => {
-                    tabs('2')
-                  }}
-                >
-                  Deliveries
-                </S.Button>
-              </div>
-            </Nav>
-            <TabContent activeTab={activeTab} className="mt-4">
-              <TabPane tabId="1" className="tabPane">
-                <Row>
-                  <Col className="col-sm-12">
-                    <S.CustomerProfile>
-                      <div>
-                        <Label>Customer</Label>
-                        <p>{customer.name}</p>
-                        <p>{customer.email}</p>
-                        <p>{customer.phone}</p>
-                      </div>
-                      <div>
-                        <div>
-                          <Label>Address</Label>
-                          <p>{customerAddress}</p>
-                        </div>
-                        <div>
-                          <Label>Payment Method</Label>
-                          Credit Card <br />
-                          DOUGLAS SANTOS <br />
-                          XXXX XXXX XXXX 9823
-                        </div>
-                      </div>
-                    </S.CustomerProfile>
-                  </Col>
-                </Row>
-              </TabPane>
-              <TabPane tabId="2" className="tabPane">
-                <Row>
-                  <Col sm="6">Content here</Col>
-                  <Col sm="6">Content here</Col>
-                </Row>
-              </TabPane>
-            </TabContent>
-          </S.Container>
-        </td>
-      </Collapse>
+          <div>
+            <S.Button
+              className={activeTab === '2' ? 'active' : ''}
+              onClick={() => {
+                tabs('2')
+              }}
+            >
+              Deliveries
+            </S.Button>
+          </div>
+        </Nav>
+        <TabContent activeTab={activeTab} className="mt-4">
+          <TabPane tabId="1" className="tabPane">
+            <Row>
+              <Col className="col-sm-12">
+                <S.CustomerProfile>
+                  <div>
+                    <Label>Customer</Label>
+                    <p>{data.name}</p>
+                    <p>{data.email}</p>
+                    <p>{data.phone}</p>
+                  </div>
+                  <div>
+                    <div>
+                      <Label>Address</Label>
+                      <p>{customerAddress}</p>
+                    </div>
+                    <div>
+                      <Label>Payment Method</Label>
+                      Credit Card <br />
+                      DOUGLAS SANTOS <br />
+                      XXXX XXXX XXXX 9823
+                    </div>
+                  </div>
+                </S.CustomerProfile>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2" className="tabPane">
+            <Row>
+              <Col sm="6">Content here</Col>
+              <Col sm="6">Content here</Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </S.Container>
     </>
   )
 }
 
-const CustomerComponent = () => {
-  const { value, handleBlock } = useFetch(GET_CUSTOMERS)
+const CustomerComponent = ({ value, loading, select, handleBlock }) => {
+  const dataFilter = value
+    ? value.filter((value) =>
+        value.status.includes(
+          `${select === 'all'.toLocaleLowerCase() ? '' : select}`
+        )
+      )
+    : value
+
+  const columns = useMemo(
+    () => [
+      {
+        name: 'Customer',
+        cell: (row) => <CustomerDetails Image={Image} name={row.name} />,
+        minWidth: '230px',
+        wrap: false
+      },
+      {
+        name: 'email',
+        selector: (row) => row['email'],
+        center: true
+      },
+      {
+        name: 'Contact',
+        cell: (row) => row['phone']
+      },
+      {
+        name: 'Requested in',
+        cell: (row) => {
+          const [date] = dateFormat(row.created_at).split(' ')
+          return date
+        }
+      },
+      {
+        name: 'Orders',
+        cell: (row) => <p>2</p>
+      }
+    ],
+    []
+  )
 
   return (
     <>
-      <Table borderless responsive>
-        <thead>
-          <S.TableRow>
-            <th scope="col">Customer</th>
-            <th scope="col">e-mail</th>
-            <th scope="col">Contact</th>
-            <th scope="col">Registered in</th>
-            <th scope="col">Orders</th>
-          </S.TableRow>
-        </thead>
-        <tbody>
-          {value &&
-            value.map((customer) => (
-              <CustomerInfo
-                key={customer.id}
-                customer={customer}
-                handleBlock={handleBlock}
-              />
-            ))}
-        </tbody>
-      </Table>
+      {value && (
+        <DataTable
+          data={dataFilter}
+          progressPending={loading}
+          expandableRows
+          columns={columns}
+          expandOnRowClicked
+          expandableRowsComponent={CustomerInfo}
+          expandableRowsComponentProps={{ handleBlock }}
+          pagination
+          paginationRowsPerPageOptions={[5, 10, 15, 20]}
+          highlightOnHover
+        />
+      )}
     </>
   )
 }
--- src/pages/Customers/index.js
+++ src/pages/Customers/index.js
@@ -1,19 +1,32 @@
-import PartialsDashboard from '../../components/PartialsDashbord'
+import { useState } from 'react'
 
+import { useFetch } from '../../hooks/useFetch'
+import { GET_CUSTOMERS } from '../../service/api'
+
+import PartialsDashboard from '../../components/PartialsDashbord'
 import ContentHeader from '../../components/ContentHeader'
 import Content from '../../components/Content'
 import CustomerComponent from '../../components/Customer/index'
 import FormDeliveryOptions from '../../components/FormDeliveryOptions'
 
 const Customers = () => {
+  const { value, loading, handleBlock } = useFetch(GET_CUSTOMERS)
+  const [select, setSelect] = useState('')
+  const options = ['all', 'approved', 'blocked']
+
   return (
     <>
       <PartialsDashboard />
       <Content>
         <ContentHeader title="Customers">
-          <FormDeliveryOptions />
+          <FormDeliveryOptions options={options} setSelect={setSelect} />
         </ContentHeader>
-        <CustomerComponent />
+        <CustomerComponent
+          value={value}
+          loading={loading}
+          select={select}
+          handleBlock={handleBlock}
+        />
       </Content>
     </>
   )
