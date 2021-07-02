76479f7268bbbb35b231cec9fc44d64393b98c59
Mayro <deehmyller11@gmail.com>
2021-07-02 20:33:26 -0300

    refact filter component

--- src/components/FormDeliveryOptions/index.js
+++ src/components/FormDeliveryOptions/index.js
@@ -1,31 +1,21 @@
-import { useState } from 'react'
-
 import * as S from './style'
 
-import { ReactComponent as ArrowDow } from '../../assets/icons/arrow-down.svg'
-
-const FormDeliveryOptions = () => {
-  const [activeDropDown, setActiveDropDown] = useState(false)
-
-  function handleSelected({ target }) {
-    console.log(target.classList.add('ok'))
-  }
+const FormDeliveryOptions = ({ setSelect, options }) => {
+  const handleInput = ({ target }) => setSelect(target.value)
 
   return (
     <S.Wrapper>
       <input type="text" />
       <S.FilterDeliveryOptions>
-        <S.CurrentOption onClick={() => setActiveDropDown(!activeDropDown)}>
-          All
-          <ArrowDow />
-        </S.CurrentOption>
-        <S.Options className={activeDropDown ? 'active' : ''}>
-          <S.Option onClick={handleSelected}>All</S.Option>
-          <S.Option>In Route</S.Option>
-          <S.Option>Awaiting acceptance</S.Option>
-          <S.Option>Completed</S.Option>
-          <S.Option>Canceled</S.Option>
-        </S.Options>
+        <select onChange={handleInput}>
+          {options.map((option) => (
+            <option key={option}>{option}</option>
+          ))}
+          {/* <option>pending</option>
+          <option>accepted</option>
+          <option>canceled</option>
+          <option>done</option> */}
+        </select>
       </S.FilterDeliveryOptions>
     </S.Wrapper>
   )
--- src/components/FormDeliveryOptions/style.js
+++ src/components/FormDeliveryOptions/style.js
@@ -5,6 +5,7 @@ export const Wrapper = styled.div`
   align-items: center;
 
   position: relative;
+  margin-bottom: 1rem;
 
   > input {
     background: #fff;
@@ -18,53 +19,12 @@ export const Wrapper = styled.div`
 
 export const FilterDeliveryOptions = styled.div`
   margin-left: 0.5rem;
-`
 
-export const CurrentOption = styled.a`
-  cursor: pointer;
-  padding: 0.5rem;
-  display: flex;
-  align-items: center;
-
-  &:hover {
-    background: #f0f0f0;
-  }
-
-  svg {
-    margin-left: 0.5rem;
-  }
-`
-export const Options = styled.div`
-  position: absolute;
-  right: 0;
-  top: 2.4rem;
-  display: block;
-  min-width: 165px;
-  min-height: 155px;
-  height: auto;
-  padding: 1rem;
-  background: #fff;
-  opacity: 0;
-  pointer-events: none;
-  z-index: 100;
-  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.4);
-
-  &.active {
-    opacity: 1;
-    pointer-events: initial;
-  }
-`
-
-export const Option = styled.a`
-  display: block;
-  padding: 0.5rem;
-  cursor: pointer;
-
-  &:hover {
-    background: #f0f0f0;
-  }
-
-  &:nth-child(1) {
-    margin-top: 0;
+  select {
+    border: 0;
+    background: none;
+    outline: none;
+    cursor: pointer;
+    text-transform: capitalize;
   }
 `
