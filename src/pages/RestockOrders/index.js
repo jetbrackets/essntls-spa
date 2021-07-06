deec8813903010a84861e1063fadf01abeebc06e
Mayro <deehmyller11@gmail.com>
2021-07-06 13:40:19 -0300

    update restock order page

--- src/pages/RestockOrders/index.js
+++ src/pages/RestockOrders/index.js
@@ -11,6 +11,7 @@ import { useState } from 'react'
 const RestockOrders = () => {
   const { value, loading } = useFetch(RESTOCK_ORDERS)
   const [select, setSelect] = useState('')
+  const [text, setText] = useState('')
   const options = [
     'all',
     'requested',
@@ -25,12 +26,17 @@ const RestockOrders = () => {
       <PartialsDashboard />
       <Content>
         <ContentHeader title="Restock Orders">
-          <FormDeliveryOptions setSelect={setSelect} options={options} />
+          <FormDeliveryOptions
+            setSelect={setSelect}
+            options={options}
+            setText={setText}
+          />
         </ContentHeader>
         <RestockOrderComponent
           value={value}
           loading={loading}
           select={select}
+          text={text}
         />
       </Content>
     </>
